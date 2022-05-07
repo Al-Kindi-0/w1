b48611281c9ab719ab101c4853ee5ff6abb1477a
Al-Kindi-0 <oamine0101@gmail.com>
2022-05-07 23:55:03 +0200

    first try

--- Q2/test/test.js
+++ Q2/test/test.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const fs = require("fs");
 const { groth16 } = require("snarkjs");
+const { plonk } = require("snarkjs");
 
 function unstringifyBigInts(o) {
     if ((typeof(o) == "string") && (/^[0-9]+$/.test(o) ))  {
@@ -49,7 +50,8 @@ describe("HelloWorld", function () {
         const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
         const c = [argv[6], argv[7]];
         const Input = argv.slice(8);
-
+        //console.log('b is',argv[2],argv[3],argv[4],argv[5]);
+        console.log('inputs are', argv[8]);
         expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
     });
     it("Should return false for invalid proof", async function () {
@@ -64,29 +66,77 @@ describe("HelloWorld", function () {
 
 describe("Multiplier3 with Groth16", function () {
 
+    let Verifier;
+    let verifier;
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("Multiplier3Verifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
+
     });
 
     it("Should return true for correct proof", async function () {
         //[assignment] insert your script here
+        const {proof, publicSignals} = await groth16.fullProve({"a":"1","b":"2","c":"4"},"contracts/circuits/Multiplier3/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3/circuit_final.zkey");
+        console.log("1*2*4=",publicSignals[0])
+        const editedPublicSignals = unstringifyBigInts(publicSignals);
+        const editedProof = unstringifyBigInts(proof);
+        const calldata = await groth16.exportSolidityCallData(editedProof, editedPublicSignals);
+
+        const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+
+        const a = [argv[0], argv[1]];
+        const b = [[argv[2], argv[3]], [argv[4], argv[5]]];
+        const c = [argv[6], argv[7]];
+        const Input = argv.slice(8);
+        console.log('inputs are', argv[8]);
+        expect(await verifier.verifyProof(a, b, c, Input)).to.be.true;
     });
+
     it("Should return false for invalid proof", async function () {
-        //[assignment] insert your script here
+        let a = [0, 0];
+        let b = [[0, 0], [0, 0]];
+        let c = [0, 0];
+        let d = [0]
+        expect(await verifier.verifyProof(a, b, c, d)).to.be.false;
     });
+
 });
 
 
 describe("Multiplier3 with PLONK", function () {
 
+    let Verifier;
+    let verifier;
     beforeEach(async function () {
         //[assignment] insert your script here
+        Verifier = await ethers.getContractFactory("contracts/Multiplier3VerifierPlonk.sol:PlonkVerifier");
+        verifier = await Verifier.deploy();
+        await verifier.deployed();
     });
 
     it("Should return true for correct proof", async function () {
-        //[assignment] insert your script here
+
+        const {proof, publicSignals} = await plonk.fullProve({"a":"1","b":"2","c":"4"},"contracts/circuits/Multiplier3_plonk/Multiplier3_js/Multiplier3.wasm","contracts/circuits/Multiplier3_plonk/circuit_final_plonk.zkey");
+        console.log("1*2*4=",publicSignals[0])
+        //const editedPublicSignals = unstringifyBigInts(publicSignals);
+        //const editedProof = unstringifyBigInts(proof);
+        //const calldata = await plonk.exportSolidityCallData(editedProof, editedPublicSignals);
+        //const calldata = await plonk.exportSolidityCallData(proof, publicSignals).split(',');
+        //const argv = calldata.replace(/["[\]\s]/g, "").split(',').map(x => BigInt(x).toString());
+        //console.log('all calldata',argv.length);
+        //let tmp = ["0x0000000000000000000000000000000000000000000000000000000000000008"];
+        //expect(await verifier.verifyProof(argv[0],argv[1])).to.be.true;
+        var text = fs.readFileSync("contracts/circuits/Multiplier3_plonk/call.txt", 'utf-8');
+        var calldata = text.split(',');
+        //console.log(calldata);
+        expect(await verifier.verifyProof(calldata[0], JSON.parse(calldata[1]))).to.be.true;
     });
     it("Should return false for invalid proof", async function () {
         //[assignment] insert your script here
+        let proof = [0];
+        let pubinp = [0];
+        expect(await verifier.verifyProof(proof,pubinp)).to.be.false;
     });
 });
